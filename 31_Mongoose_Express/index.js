2b4c1c11b15a2e91f91261bb24700039809138db
PMtHk <zooby88@naver.com>
2022-07-27 22:44:07 +0900

    add edit route, edit.ejs

--- 31_Mongoose_Express/index.js
+++ 31_Mongoose_Express/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-
+const methodOverride = require("method-override");
 const Product = require("./models/product");
 
 mongoose
@@ -18,6 +18,7 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
@@ -39,6 +40,21 @@ app.get("/products/:id", async (req, res) => {
   const product = await Product.findById(id);
   console.log(product);
   res.render("products/detail", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
