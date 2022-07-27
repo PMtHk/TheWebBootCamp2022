be9c5319d2eaebdbc2e0ec9771adb70998a89ede
PMtHk <zooby88@naver.com>
2022-07-27 17:55:16 +0900

    set database Model, Schema and add list page, detail page

--- 31_Mongoose_Express/index.js
+++ 31_Mongoose_Express/index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
+const Product = require("./models/product");
+
 mongoose
-  .connect("mongodb://localhost:27017/shopApp")
+  .connect("mongodb://localhost:27017/farmStand")
   .then(() => {
     console.log("Connected to Database");
   })
@@ -16,8 +18,17 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/dog", (req, res) => {
-  res.send("WOOF!");
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  // console.log(products);
+  res.render("products/index", { products });
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render("products/detail", { product });
 });
 
 app.listen(3000, () => {
---
+++ 31_Mongoose_Express/seeds.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStand")
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed");
+    console.log(err);
+  });
+
+// const p = new Product({
+//   name: "Ruby Grapefruit",
+//   price: 1.99,
+//   category: "fruit",
+// });
+
+// p.save()
+//   .then((p) => {
+//     console.log(p);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+// insertMany prac.
+const seedProducts = [
+  {
+    name: "Fairy Eggplant",
+    price: 1.0,
+    category: "vegetable",
+  },
+  {
+    name: "Organic Goddess Melon",
+    price: 4.99,
+    category: "fruit",
+  },
+  {
+    name: "Organic Mini Seedless Watermelon",
+    price: 3.99,
+    category: "fruit",
+  },
+  {
+    name: "Organic Celery",
+    price: 1.5,
+    category: "vegetable",
+  },
+  {
+    name: "Chocolate Whole Milk",
+    price: 2.69,
+    category: "dairy",
+  },
+];
+// 주의! -> insertMany에서 하나라도 유효성 검사를 통과하지 못하면, 아무것도 삽입되지 않는다.
+Product.insertMany(seedProducts)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
