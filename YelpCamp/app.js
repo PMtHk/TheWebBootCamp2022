096be7df2c8fccb6cb8b60b1c29d9cb8289e7aba
PMtHk <zooby88@naver.com>
2022-07-29 13:36:57 +0900

    use ejs-mate module & bootstrap@5.2.0

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
+const engine = require("ejs-mate");
 
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp")
@@ -16,6 +17,7 @@ mongoose
 
 const app = express();
 
+app.engine("ejs", engine);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
