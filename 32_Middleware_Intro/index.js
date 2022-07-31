fde935f2dc1b6effa213c1496aa3c7a25e47e8d1
PMtHk <zooby88@naver.com>
2022-07-31 18:14:58 +0900

    Error Handling in Express (not async)

--- 32_Middleware_Intro/index.js
+++ 32_Middleware_Intro/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+const AppError = require("./AppError");
+
 //middleware
 // app.use(morgan("dev"));
 
@@ -22,7 +24,9 @@ const verifyPassword = (req, res, next) => {
   if (password === "tempPassword") {
     next();
   }
-  res.send("Sorry, You need a pssword!");
+  // res.send("Sorry, You need a pssword!");
+  // res.status(401);
+  throw new AppError("Password required", 401);
 };
 
 // app.use((req, res, next) => {
@@ -40,6 +44,10 @@ app.get("/", (req, res) => {
   res.send("Home");
 });
 
+app.get("/error", (re, res) => {
+  chicken.fly();
+});
+
 app.get("/dogs", (req, res) => {
   res.send("WOOF WOOF");
 });
@@ -48,8 +56,25 @@ app.get("/secret", verifyPassword, (req, res) => {
   res.send("About Secret");
 });
 
+app.get("/admin", (req, res) => {
+  throw new AppError("You are not an Admin!", 403);
+});
+
 app.use((req, res) => {
   res.status(404).send("NOT FOUND!");
+});
+
+// app.use((err, req, res, next) => {
+//   console.log("********************************************");
+//   console.log("********************ERROR*******************");
+//   console.log("********************************************");
+//   // res.status(500).send("Error...");
+//   next(err); // 오류를 내장 error 핸들러에게 넘기는 방법.
+// });
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something Went Wrong!" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
