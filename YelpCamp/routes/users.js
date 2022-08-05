693c8b83b88623511afd94c947fcb7c795ebf8cc
PMtHk <zooby88@naver.com>
2022-08-05 16:37:52 +0900

    Refactoring Users with Controller

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,36 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
-const { register } = require('../models/user');
 const passport = require('passport');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-});
+router.get('/register', users.renderRegisterForm);
 
-router.post(
-  '/register',
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds');
-      });
-    } catch (e) {
-      req.flash('error', e.message);
-      res.redirect('/register');
-    }
-  })
-);
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
+router.get('/login', users.renderLoginForm);
 
 router.post(
   '/login',
@@ -39,12 +17,7 @@ router.post(
     failureRedirect: '/login',
     keepSessionInfo: true, // session reset 을 막자!
   }),
-  (req, res) => {
-    req.flash('success', 'Welcome Back!');
-    // console.log(req.session);
-    res.redirect(req.session.returnTo || '/campgrounds');
-    delete req.session.returnTo;
-  }
+  users.login
 );
 
 // passport ~0.5
@@ -55,12 +28,6 @@ router.post(
 // });
 
 // passport 0.6~
-router.get('/logout', (req, res, next) => {
-  req.logout((err) => {
-    if (err) return next(err);
-    req.flash('success', 'Goodbye!');
-    res.redirect('/campgrounds');
-  });
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
