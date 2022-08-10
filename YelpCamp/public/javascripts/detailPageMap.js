9346568532fd8c007f84b45112bcce80493dadd4
PMtHk <zooby88@naver.com>
2022-08-10 18:13:34 +0900

    add popup to map

--- YelpCamp/public/javascripts/detailPageMap.js
+++ YelpCamp/public/javascripts/detailPageMap.js
@@ -1,10 +1,20 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: (campground.geometry.coordinates === undefined ? [126.9784147, 37.5666805] : campground.geometry.coordinates), // starting position [lng, lat]
+  style: 'mapbox://styles/mapbox/outdoors-v10', // style URL
+  center:
+    campground.geometry.coordinates === undefined
+      ? [126.9784147, 37.5666805]
+      : campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 11, // starting zoom
   projection: 'globe', // display the map as a 3D globe
 });
 
-// new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h5>${campground.title}</h5><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
